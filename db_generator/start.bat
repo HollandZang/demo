36b8237c97f86be764564b647112457910da8eca
holland <zhn.pop@gmail.com>
2020-12-16 23:40:26 +0800

    2020年12月16日，学习成果

--- db_generator/start.bat
+++ db_generator/start.bat
@@ -4,56 +4,66 @@
 for /f "tokens=1,2 delims= " %%a in (db.conf) do (
     set %%a=%%b
 )
-
 @rem Complete init_data
-if %url%==() (set /p url=Plz input 'url' ^(such: jdbc:oracle:thin:@localhost:1521/orcl^)^: )^
-else echo Read from db.conf: url=%url%
-if %user%==() (set /p user=Plz input 'user' : )^
-else echo Read from db.conf: user=%user%
-if %pwd%==() (set /p pwd=Plz input 'pwd' : )^
-else echo Read from db.conf: pwd=%pwd%
-if %package%==() (set /p package=Plz input 'package' : )^
-else echo Read from db.conf: package=%package%
+if (%url%)==() (
+    set /p url=Plz input 'url' :
+) else (
+    echo Read from db.conf: url=%url%
+)
+if (%user%)==() (
+    set /p user=Plz input 'user' :
+) else (
+    echo Read from db.conf: user=%user%
+)
+if (%pwd%)==() (
+    set /p pwd=Plz input 'pwd' :
+) else (
+    echo Read from db.conf: pwd=%pwd%
+)
+if (%package%)==() (
+    set /p package=Plz input 'package' :
+) else (
+    echo Read from db.conf: package=%package%
+)
+echo url       %url%>db.conf
+echo user      %user%>>db.conf
+echo pwd       %pwd%>>db.conf
+echo package   %package%>>db.conf
+echo.
 
 @rem Input command to generate
 :command
 echo Plz input command ^(input 'helper' to quick started^)^:
-set /p command=
+set /p line=
+echo.
 
 @rem sys_command
-if %command%==helper goto helper
-if %command%==exit goto end
+if "%line%"=="helper" goto helper
+if "%line%"=="exit" goto end
 
 @rem db_command
-if %command%==tables goto tables
+if "%line%"=="tables" goto tables
 
 @rem generate
-for /f "delims= " %%a in("%command%") do(
-  if
-  set table_name=%%a
-  set password=%%b
+setlocal enabledelayedexpansion
+for /f "tokens=1,* delims= " %%a in ("%line%") do (
+    set table_name=%%a
+    set params=%%b
+    goto execute
 )
+:execute
+set command=generate
+call task.bat %command% %url% %user% %pwd% %table_name% %package%
 
-::set /p table_name=Plz input table_name:
-::echo %table_name%
-::java -jar ../build/libs/demo-1.0-SNAPSHOT.jar %url% %user% %pwd% %table_name% %package%
+goto command
 
 :helper
-echo.
-echo Helper
-echo    System command:
-echo        helper     user guide
-echo        exit       exit this cmd
-echo    Database command:
-echo        tables     get the name of all the tables
-echo    Generate parameter: ^<database table name^> [params...]
-echo        -a,-all    generate all, include 'pojo', etc...
-echo        -m,-model  generate pojo
-echo.
+cat ".helper"
 pause
 goto command
 
 :tables
-java -jar ../build/libs/demo-1.0-SNAPSHOT.jar tables %url% %user% %pwd%
+set command=tables
+call task.bat %command% %url% %user% %pwd%
 
 :end
---
+++ db_generator/task.bat
@@ -0,0 +1,12 @@
+@echo off
+
+if %command%==tables (
+    java -jar ../build/libs/demo-1.0-SNAPSHOT.jar tables %url% %user% %pwd%
+) else if %command%==generate (
+    java -jar ../build/libs/demo-1.0-SNAPSHOT.jar generate %url% %user% %pwd% %table_name% %package%
+) else (
+    echo "todo 都走到了generate"
+)
+
+echo task finished
+echo.
